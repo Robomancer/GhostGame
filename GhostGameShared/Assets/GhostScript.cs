ed75959b8b741e59d461bb09f4ef3fc0d7f3bf86
WilfHeim <55141670+WilfHeim@users.noreply.github.com>
2022-08-08 12:07:38 -0400

    Ghost - 1
    Basic Ghost Functionality for exchanging items

---
+++ GhostGameShared/Assets/GhostScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostScript : MonoBehaviour
+{
+    private enum GhostType { King, Queen, Knight, Archer, Bard, Mage };
+    private string FavoriteItem;
+    private string Name;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag(FavoriteItem)) 
+        {
+
+        }
+    }
+}
